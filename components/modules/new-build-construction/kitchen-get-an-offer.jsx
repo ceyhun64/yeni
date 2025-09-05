173a1013b6fd37c4a1dd62c4f948558196f1cdc7
Ceyhun Turkmen <ctrkmn64@gmail.com>
2025-09-05 21:39:58 +0300

    8. commit

--- components/modules/new-build-construction/kitchen-get-an-offer.jsx
+++ components/modules/new-build-construction/kitchen-get-an-offer.jsx
@@ -130,6 +130,7 @@ export default function GetAnOffer() {
                 />
               ))}
             </div>
+            {/* ejdwkşjdkewjdejdjwekdjkewhdkjwhdkjewıde */}
             {selectedImage && (
               <div
                 className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
